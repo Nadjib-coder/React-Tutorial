c41b80c625317fe01c122c83125b36fce0377048
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-02-09 15:00:58 +0000

    complete the header section

---
+++ projects/chef_claude/src/components/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <>
+      <div className="header-container">
+        <img className="header-image" src="icon.png" alt="chef claude" />
+        <h1 className="header-title">Chef Claude</h1>
+      </div>
+    </>
+  );
+}
