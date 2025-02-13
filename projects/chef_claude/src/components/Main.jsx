25c8bb85efdaa265ae9eaf7ce2b44fde50b164c6
Nadjib-coder <nadjib.programmation1@gmail.com>
2025-02-13 14:42:05 +0000

    update Main.jsx file - add react hooks 'useState()' to handle the user input

--- projects/chef_claude/src/components/Main.jsx
+++ projects/chef_claude/src/components/Main.jsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
+
 export default function Main() {
-  const handelClick = () => {
-    alert('You clicked me!');
-  };
+  const [ingredients, setIngredients] = useState([]);
+
+  const ingredientListItem = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const newValue = formData.get('ingredient');
+    setIngredients((prev) => [...prev, newValue]);
+  }
+
   return (
     <main>
-      <form className="add-ingredient-form">
+      <form onSubmit={handleSubmit} className="add-ingredient-form">
         <input
           type="text"
           placeholder="e.g. oregano"
           aria-label="Add Ingredient"
+          name="ingredient"
         ></input>
-        <button onClick={handelClick}>Add ingredient</button>
+        <button>Add ingredient</button>
       </form>
+      <ul>{ingredientListItem}</ul>
     </main>
   );
 }
